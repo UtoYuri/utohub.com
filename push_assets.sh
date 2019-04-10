819a2c56b4b83e7b726381595cb195081781f264
‘yuri’ <yuri@utohub.com>
2019-04-10 20:36:21 +0800

    upload assets to cos

---
+++ push_assets.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+pip install coscmd==1.8.5.28
+
+coscmd config -a ${SECRET_ID} -s ${SECRET_KEY} -b ${BUCKET} -r ${REGION}
+coscmd upload -r ./data/ /utohub/home/data
+coscmd upload -r ./build/ /utohub/home/assets
